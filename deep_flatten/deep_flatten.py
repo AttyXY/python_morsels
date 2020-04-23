df98a903289523b0c6d9d601411defc21210de51
attyxy <attyxy@gmail.com>
2020-04-23 03:52:32 -0400

    FEAT: deep_flatten base, bonus1

--- deep_flatten/deep_flatten.py
+++ deep_flatten/deep_flatten.py
@@ -1,18 +1,23 @@
-from typing import List
+from typing import Iterable
 
-def deep_flatten(lst: List[List]):
+def deep_flatten(iterObj: Iterable) -> Iterable:
     """Returns a flattened version of lst
 
+    Base: Accept lists
     >>> deep_flatten([[(1, 2), (3, 4)], [(5, 6), (7, 8)]])
     [1, 2, 3, 4, 5, 6, 7, 8]
     >>> deep_flatten([[1, [2, 3]], 4, 5])
     [1, 2, 3, 4, 5]
+
+    Bonus 1: Accept all iterables but strings
+    >>> sorted(deep_flatten({(1, 2), (3, 4), (5, 6), (7, 8)}))
+    [1, 2, 3, 4, 5, 6, 7, 8]
     """
     flattened_list = []
-    for elem in lst:
-        if not hasattr(elem, "__iter__"):
-            flattened_list.append(elem)
+    for elem in iterObj:
+        if hasattr(elem, "__iter__"):
+            flattened_list.extend(deep_flatten(elem))
         else:
-            flattened_list.append(deep_flatten(elem))
+            flattened_list.append(elem)
 
     return flattened_list

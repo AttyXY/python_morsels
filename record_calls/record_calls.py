6332470cd13ea283cca6658c126a79b465842948
attyxy <attyxy@gmail.com>
2020-04-27 03:00:39 -0400

    FEAT: record_calls bonus1 + bonus2.

--- record_calls/record_calls.py
+++ record_calls/record_calls.py
@@ -1,4 +1,6 @@
 from typing import List, Dict
+from functools import wraps
+from dataclasses import dataclass
 
 
 # Base
@@ -8,7 +10,7 @@ def record_calls(func):
         func.call_count += 1
         return func(*args, **kwargs)
 
-    wrapper.call_count = 0
+    func.call_count = 0
     return wrapper
 
 @record_calls
@@ -23,5 +25,61 @@ def greet(name="world"):
     Hello world
     >>> greet.call_count
     2
+    """
+    print(f"Hello {name}")
+
+
+# Bonus 1
+def record_calls(func):
+    """Records the number of times func is called. Keeps func metadata."""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.call_count += 1
+        return func(*args, **kwargs)
+
+    wrapper.call_count = 0
+    return wrapper
+
+@record_calls
+def greet(name="world"):
+    """Greet someone by their name."""
+    print(f"Hello {name}")
+
+
+# Bonus 2
+@dataclass
+class Call:
+    args: List
+    kwargs: Dict
+
+def record_calls(func):
+    """Records the number of times func is called."""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.call_count += 1
+        wrapper.calls.append(Call(args=args, kwargs=kwargs))
+        return func(*args, **kwargs)
+
+    wrapper.call_count = 0
+    wrapper.calls = []
+    return wrapper
+
+
+@record_calls
+def greet(name="world"):
+    """Greet someone by their name.
+
+    >>> greet("Atty")
+    Hello Atty
+    >>> greet.calls[0].args
+    ('Atty',)
+    >>> greet.calls[0].kwargs
+    {}
+    >>> greet(name="Atty")
+    Hello Atty
+    >>> greet.calls[1].args
+    ()
+    >>> greet.calls[1].kwargs
+    {'name': 'Atty'}
     """
     print(f"Hello {name}")

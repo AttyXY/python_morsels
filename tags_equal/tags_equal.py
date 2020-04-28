b39a181caf049c83c8d92137113cc62b0e5ab8a6
attyxy <attyxy@gmail.com>
2020-04-27 21:19:59 -0400

    ENH: tags_equal bonus 1

--- tags_equal/tags_equal.py
+++ tags_equal/tags_equal.py
@@ -61,3 +61,66 @@ def tags_equal(tag1: str, tag2: str) -> bool:
     False
     """
     return HTMLTag(tag1) == HTMLTag(tag2)
+
+
+# Bonus 1
+class InvalidHTMLTagException(Exception):
+    pass
+
+class HTMLTag:
+    def __init__(self, tag):
+        self.og_tag = tag
+        self.tagname = ""
+        self.attrs = {}
+
+        # valid html tag?
+        closed = tag.startswith('<') and tag.endswith('>')
+        has_tagname = len(tag) >= 3
+        if closed and has_tagname:
+            tag = tag[1:-1]     # strip open and closing brackets
+            self.tagname = tag.split(' ', 1)[0].lower()    # get tagname
+        else:
+            raise InvalidHTMLTagException
+
+        # parse attributes
+        try:
+            tag = tag.split(' ', 1)[1]
+            attrs = tag.split('=')
+        except IndexError:
+            return  # no attributes
+
+        key = attrs[0]
+        for i in range(1, len(attrs)):
+            temp = attrs[i].split(' ')
+            val = temp[0]
+            if key not in self.attrs:   # only record first instance
+                self.attrs[key.lower()] = val.lower()
+            try:
+                key = ' '.join(temp[1:])
+            except IndexError:
+                pass    # reached last key
+
+
+    def __eq__(self, other):
+        if self.tagname != other.tagname:
+            return False
+
+        for k,v in self.attrs.items():
+            try:
+                if other.attrs[k] != v:
+                    return False    # value not the same
+            except KeyError:
+                return False    # does not contain key
+        return True
+
+
+def tags_equal(tag1: str, tag2: str) -> bool:
+    """Returns True if 2 HTML tags have the same attributes and values.
+    Ignores duplicate attributes.
+
+    >>> tags_equal("<LABEL FOR=id_email for=id_username>", "<LABEL FOR=id_email>")
+    True
+    >>> tags_equal("<LABEL FOR=id_email for=id_username>", "<LABEL FOR=id_username>")
+    False
+    """
+    return HTMLTag(tag1) == HTMLTag(tag2)

cddba0332e1fb9cf9362dd0e8981bb02705f0085
attyxy <attyxy@gmail.com>
2020-04-29 23:12:04 -0400

    FEAT(format_ranges): bonus 1

--- format_ranges/format_ranges.py
+++ format_ranges/format_ranges.py
@@ -10,13 +10,19 @@ def format_ranges(nums: List[int]) -> str:
     >>> format_ranges([1, 2, 3, 5, 6, 7, 8, 10, 11])
     '1-3,5-8,10-11'
 
+    bonus 1: handle single numbers
+    >>> format_ranges([4])
+    '4'
+    >>> format_ranges([1, 3, 5, 6, 8])
+    '1,3,5-6,8'
+
     custom
     >>> format_ranges([])
     ''
     """
-    ranges = []
+    ranges = [[]]
     try:
-        ranges.append(str(nums[0]))
+        ranges[-1].append(nums[0])
     except IndexError:
         return ""   # empty list
 
@@ -24,10 +30,14 @@ def format_ranges(nums: List[int]) -> str:
         prev_num = nums[i - 1]
         curr_num = nums[i]
         if curr_num != prev_num + 1:
-            ranges[-1] = f"{ranges[-1]}-{prev_num}"
-            ranges.append(str(curr_num))
-        elif i == len(nums) - 1:
-            ranges[-1] = f"{ranges[-1]}-{curr_num}"
+            ranges.append([curr_num])
+        else:
+            ranges[-1].append(curr_num)
 
+    def range_to_str(subrange):
+        if subrange[0] == subrange[-1]:
+            return f"{subrange[0]}"
+        else:
+            return f"{subrange[0]}-{subrange[-1]}"
 
-    return ','.join(ranges)
+    return ','.join([range_to_str(subrange) for subrange in ranges])

ec605f19a8e99df9472a35695bd4e213c25e6471
attyxy <attyxy@gmail.com>
2020-04-30 16:19:40 -0400

    BUG(FuzzyString): added <= and >= operators

--- FuzzyString/FuzzyString.py
+++ FuzzyString/FuzzyString.py
@@ -17,15 +17,47 @@ class FuzzyString(str):
     True
     >>> 'hashtag' > o_word
     False
+
+    custom
+    >>> test = FuzzyString('B')
+    >>> test == 'b'
+    True
+    >>> test == 'A'
+    False
+
+    >>> test < 'b'
+    False
+    >>> test < 'b'
+    False
+    >>> test < 'a'
+    False
+    >>> test > 'a'
+    True
+
+    >>> test <= 'b'
+    True
+    >>> test >= 'b'
+    True
+    >>> test <= 'a'
+    False
+    >>> test >= 'a'
+    True
+
     """
     def __eq__(self, other: str):
         return self.lower() == other.lower()
-
-    def __ne__(self, other: str):
-        return not self.__eq__(other)
 
     def __lt__(self, other: str):
         return self.lower() < other.lower()
 
     def __gt__(self, other: str):
         return self.lower() > other.lower()
+
+    def __ne__(self, other: str):
+        return not self.__eq__(other)
+
+    def __le__(self, other: str):
+        return self.__eq__(other) or self.__lt__(other)
+
+    def __ge__(self, other: str):
+        return self.__eq__(other) or self.__gt__(other)
